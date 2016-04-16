95bb6c9f3a1388dffe1c540a711a3755b0b2f30e
everardo sanchez <everardo.ipn@gmail.com>
2016-04-16 13:35:02 -0500

    feat: defines server resopnse class

---
+++ app/src/main/java/com/example/evesan/servertemp/ServerResponse.java
@@ -0,0 +1,34 @@
+package com.example.evesan.servertemp;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+
+/**
+ * Created by evesan on 4/16/16.
+ */
+public class ServerResponse extends Thread {
+
+    Socket client;
+
+    public ServerResponse(Socket clientSocket) {
+        super("ServerResponse");
+        this.client = clientSocket;
+    }
+
+    public void run() {
+        OutputStream outStream;
+        String replyMessage = "OK";
+
+        try {
+            outStream = this.client.getOutputStream();
+            PrintStream pStream = new PrintStream(outStream);
+            pStream.print("OK\n");
+            pStream.close();
+        }
+        catch (IOException e) {
+
+        }
+    }
+}
